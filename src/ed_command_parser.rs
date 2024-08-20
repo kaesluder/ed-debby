6a577b50448703de39c25f53b617631f80018f36
Kae Sluder <kaesluder@fastmail.com>
2024-08-20 19:56:29 -0400

    refactor address parser for better error handling; add docstrings

--- src/ed_command_parser.rs
+++ src/ed_command_parser.rs
@@ -1,10 +1,18 @@
-use pest::Parser;
+use pest::{error::Error, Parser};
 use pest_derive::Parser;
 
 #[derive(Parser)]
 #[grammar = "ed_command.pest"] // Adjust the grammar path as necessary
 pub struct EdCommandParser;
 
+/// Represents an address in the `ed` editor.
+///
+/// # Values
+///
+/// * `Abolute(usize)` - An absolute one-indexed linenumber reference.
+/// * `Last` - The last line in the buffer (`$`).
+/// * `Current` - Current line in buffer (`.`, default for most cases).
+/// * `None` - No address.
 #[derive(Debug, Eq, PartialEq, Clone)]
 enum Address {
     Absolute(usize),
@@ -14,21 +22,63 @@ enum Address {
 }
 
 impl Address {
+    /// Parses a string representing an `ed` address, returning an `Address` enum.
+    ///
+    /// # Arguments
+    ///
+    /// * `input` - string slice of address from `ed` command string
+    ///
+    /// # Returns
+    ///
+    /// * `Option<Address>` - Returns `Option` containing an `Address` enum. `None` if string cannot be parsed to `usize`.
+    ///
     fn from_str(input: &str) -> Option<Self> {
         match input {
             "." => Some(Address::Current),
             "$" => Some(Address::Last),
-            _ => input.parse::<usize>().ok().map(Address::Absolute),
+            _ => input.parse::<usize>().ok().map(Address::Absolute), // returns `None` on parse error
         }
     }
 }
 
+/// Represents an `ed` range separator.
+///
+/// # Values
+///
+/// * `Comma` - Preserves current line.
+/// * `Semicolon` - Sets current line to first value in range.
 #[derive(Debug, Eq, PartialEq)]
 enum RangeSep {
     Comma,
     Semicolon,
 }
 
+/// Represents an ed command with optional addresses, a range separator,
+/// and an optional command with arguments.
+///
+/// The `EdCommand` struct is used to represent a parsed command in an `ed`-style text editor.
+/// It includes two addresses that specify a range, a separator between those addresses,
+/// and an optional command with associated arguments.
+///
+/// # Fields
+///
+/// * `address1` - The first address in the command. This can represent the starting point of a range or a single address.
+/// * `address2` - The second address in the command. This can represent the end point of a range.
+/// * `range_sep` - The separator used between the two addresses, typically a comma (`,`) or semicolon (`;`).
+/// * `command` - An optional `String` representing the command to be executed.
+/// * `command_args` - An optional `String` representing the arguments to the command.
+///
+/// # Example
+///
+/// ```rust
+/// let cmd = EdCommand {
+///     address1: Address::Absolute(1),
+///     address2: Address::Absolute(5),
+///     range_sep: RangeSep::Comma,
+///     command: Some("d".to_string()),
+///     command_args: None,
+/// };
+/// ```
 #[derive(Debug, Eq, PartialEq)]
 struct EdCommand {
     address1: Address,
@@ -38,9 +88,34 @@ struct EdCommand {
     command_args: Option<String>,
 }
 
-fn parse_range(input: &str) -> (Address, RangeSep, Address) {
-    let pairs = EdCommandParser::parse(Rule::range, input)
-        .expect("unsuccessful parse")
+/// Parses a string input into a tuple representing a range of addresses with a separator.
+///
+/// # Arguments
+///
+/// * `input` - A string slice that holds the input to be parsed. This input should represent
+/// a range in the form of two addresses separated by a comma (`,`), semicolon (`;`), or no separator.
+///
+/// # Returns
+///
+/// * `Result<(Address, RangeSep, Address), pest::error::Error<crate::ed_command_parser::Rule>>` -
+///   Returns a `Result` containing a tuple with the first address, the range separator, and
+///   the second address, or a `pest::error::Error` if parsing fails.
+///
+/// # Example
+///
+/// ```rust
+/// let result = parse_range("10,20");
+/// assert_eq!(result.unwrap(), (Address::Absolute(10), RangeSep::Comma, Address::Absolute(20)));
+/// ```
+///
+/// # Errors
+///
+/// This function returns a `pest::error::Error<crate::ed_command_parser::Rule>` if the input does not
+/// conform to the expected format for a range, which is defined by the `Rule::range` in the `pest` parser.
+fn parse_range(
+    input: &str,
+) -> Result<(Address, RangeSep, Address), pest::error::Error<crate::ed_command_parser::Rule>> {
+    let pairs = EdCommandParser::parse(Rule::range, input)?
         .next()
         .unwrap()
         .into_inner();
@@ -77,7 +152,7 @@ fn parse_range(input: &str) -> (Address, RangeSep, Address) {
         address2 = address1.clone();
     }
 
-    (address1, separator, address2)
+    Ok((address1, separator, address2))
 }
 
 #[cfg(test)]
@@ -116,10 +191,12 @@ mod tests {
         #[case] input: &str,
         #[case] expected: (Address, RangeSep, Address),
         #[case] note: &str,
-    ) {
-        let range = parse_range(input);
-        assert_eq!(range, expected, "{}", note)
+    ) -> Result<(), Error<Rule>> {
+        let range = parse_range(input)?;
+        assert_eq!(range, expected, "{}", note);
+        Ok(())
     }
+
     #[rstest]
     #[case("50", Address::Absolute(50), "'50' matches 50")]
     #[case("5", Address::Absolute(5), "'5' matches 5")]
